5004e6651dcfbd02add5670f07378555e23cc99d
John Lewis <jlewis@forsta.io>
2017-01-19 15:47:52 -0700

    JSON formatted message to SM

--- src/io/forsta/ccsm/CcsmSync.java
+++ src/io/forsta/ccsm/CcsmSync.java
@@ -3,6 +3,9 @@ package io.forsta.ccsm;
 import android.content.Context;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.thoughtcrime.securesms.BuildConfig;
 import org.thoughtcrime.securesms.crypto.MasterSecret;
 import org.thoughtcrime.securesms.crypto.MasterSecretUnion;
@@ -10,10 +13,13 @@ import org.thoughtcrime.securesms.database.DatabaseFactory;
 import org.thoughtcrime.securesms.database.EncryptingSmsDatabase;
 import org.thoughtcrime.securesms.database.NoSuchMessageException;
 import org.thoughtcrime.securesms.database.model.SmsMessageRecord;
+import org.thoughtcrime.securesms.recipients.Recipient;
 import org.thoughtcrime.securesms.recipients.RecipientFactory;
 import org.thoughtcrime.securesms.recipients.Recipients;
 import org.thoughtcrime.securesms.sms.MessageSender;
 import org.thoughtcrime.securesms.sms.OutgoingTextMessage;
+
+import java.util.List;
 
 /**
  * Created by jlewis on 1/18/17.
@@ -33,6 +39,8 @@ public class CcsmSync {
 
         try {
             SmsMessageRecord message = database.getMessage(masterSecret, messageId);
+            JSONObject jsonBody = createMessageBody(message);
+            // TODO send this shit to the SM.
             OutgoingTextMessage superMessage = new OutgoingTextMessage(superRecipients, message.getDisplayBody().toString(), message.getExpiresIn(), message.getSubscriptionId());
             // Hide thread from UI.
             long superThreadId = -1;
@@ -43,11 +51,28 @@ public class CcsmSync {
 
             long superMessageId = database.insertMessageOutbox(new MasterSecretUnion(masterSecret), superThreadId, superMessage, false, System.currentTimeMillis());
             MessageSender.sendTextMessage(context, superRecipients, false, false, superMessageId, superMessage.getExpiresIn());
-            Log.d(TAG, "Forsta Sync. Message Sent.");
+            Log.d(TAG, "Forsta Sync. Sending Sync Message.");
         } catch (NoSuchMessageException e) {
             e.printStackTrace();
-            Log.e(TAG, "Forsta Sync message not sent");
-            // TODO handle failed messages.
+            Log.e(TAG, "Forsta Sync message not found.");
         }
+    }
+
+    private static JSONObject createMessageBody(SmsMessageRecord message) {
+        JSONObject json = new JSONObject();
+        Recipients recipients = message.getRecipients();
+        List<Recipient> list = recipients.getRecipientsList();
+        JSONArray dest = new JSONArray();
+        for (Recipient item : list) {
+            dest.put(item.getNumber());
+        }
+        try {
+            json.put("dest", dest);
+            json.put("message", message.getDisplayBody().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return json;
     }
 }
