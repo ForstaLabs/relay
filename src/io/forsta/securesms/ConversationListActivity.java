0a15e540b1837c27ccae7426e2812d727831199c
John Lewis <jlewis@forsta.io>
2017-05-05 12:14:08 -0600

    Small fixes

--- src/io/forsta/securesms/ConversationListActivity.java
+++ src/io/forsta/securesms/ConversationListActivity.java
@@ -167,7 +167,7 @@ public class ConversationListActivity extends PassphraseRequiredActionBarActivit
     case R.id.menu_settings:          handleDisplaySettings(); return true;
 //    case R.id.menu_clear_passphrase:  handleClearPassphrase(); return true;
 //    case R.id.menu_mark_all_read:     handleMarkAllRead();     return true;
-//    case R.id.menu_import_export:     handleImportExport();    return true;
+    case R.id.menu_import_export:     handleImportExport();    return true;
 //    case R.id.menu_invite:            handleInvite();          return true;
     case R.id.menu_help:              handleHelp();            return true;
     case R.id.menu_directory:         handleDirectory();       return true;
