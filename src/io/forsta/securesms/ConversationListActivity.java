b936f037ef0d142ff32245228741554a178a261d
John Lewis <jlewis@forsta.io>
2017-05-22 10:13:24 -0600

    Remove ability to create groups locally outside of tag composition

--- src/io/forsta/securesms/ConversationListActivity.java
+++ src/io/forsta/securesms/ConversationListActivity.java
@@ -261,7 +261,7 @@ public class ConversationListActivity extends PassphraseRequiredActionBarActivit
     super.onOptionsItemSelected(item);
 
     switch (item.getItemId()) {
-    case R.id.menu_new_group:         createGroup();           return true;
+//    case R.id.menu_new_group:         createGroup();           return true;
     case R.id.menu_settings:          handleDisplaySettings(); return true;
     case R.id.menu_clear_passphrase:  handleClearPassphrase(); return true;
     case R.id.menu_mark_all_read:     handleMarkAllRead();     return true;
