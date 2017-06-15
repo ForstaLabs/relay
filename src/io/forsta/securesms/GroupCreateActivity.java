dc45828465a107352c2b901fea3ef680ea1a6f50
John Lewis <jlewis@forsta.io>
2017-06-15 10:48:13 -0600

    Remove commented code

--- src/io/forsta/securesms/GroupCreateActivity.java
+++ src/io/forsta/securesms/GroupCreateActivity.java
@@ -165,10 +165,6 @@ public class  GroupCreateActivity extends PassphraseRequiredActionBarActivity
       getSupportActionBar().setTitle(groupToUpdate.isPresent()
                                      ? R.string.GroupCreateActivity_actionbar_update_title
                                      : R.string.GroupCreateActivity_actionbar_title);
-//      if (groupToUpdate.isPresent()) {
-//        GroupData data = groupToUpdate.get();
-//        groupName.setEnabled(false);
-//      }
     }
   }
 
