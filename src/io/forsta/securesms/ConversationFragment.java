75c49bb0c10a2d1f940e9313a17e32da8bd0d3b1
John Lewis <jlewis@forsta.io>
2017-07-18 09:09:14 -0600

    Fix bug in message forwarding

--- src/io/forsta/securesms/ConversationFragment.java
+++ src/io/forsta/securesms/ConversationFragment.java
@@ -34,6 +34,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ItemAnimator.ItemAnimatorFinishedListener;
 import android.text.ClipboardManager;
+import android.text.Spannable;
+import android.text.Spanned;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -325,14 +327,14 @@ public class ConversationFragment extends Fragment
   }
 
   private String getMessageBody(String body) {
-    String forstaBody = ForstaUtils.getForstaJsonBody(body).toString();
+    Spanned forstaBody = ForstaUtils.getForstaJsonBody(body);
     if (forstaBody == null) {
-      forstaBody = ForstaUtils.getForstaPlainTextBody(body).toString();
+      String plainBody = ForstaUtils.getForstaPlainTextBody(body);
       if (forstaBody == null) {
-        forstaBody = body;
+        return body;
       }
     }
-    return forstaBody;
+    return forstaBody.toString();
   }
 
   private void handleForwardMessage(MessageRecord message) {
