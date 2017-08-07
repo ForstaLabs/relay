a981d9bc7539d6c3ecd39151772851deda61fd57
Justin Mayfield <tooker@gmail.com>
2017-08-07 17:21:52 -0600

    Rip out the registration activity (and friends)
    Taking out the trash...

--- src/io/forsta/securesms/RegistrationActivity.java
+++ src/io/forsta/securesms/RegistrationActivity.java
@@ -1,3 +1,4 @@
+// vim: ts=2:sw=2:expandtab
 package io.forsta.securesms;
 
 import android.content.ActivityNotFoundException;
@@ -42,282 +43,25 @@ import org.whispersystems.signalservice.api.util.PhoneNumberFormatter;
  */
 public class RegistrationActivity extends BaseActionBarActivity {
 
-  private static final int PICK_COUNTRY = 1;
-  private static final String TAG = RegistrationActivity.class.getSimpleName();
-
-  private AsYouTypeFormatter   countryFormatter;
-  private ArrayAdapter<String> countrySpinnerAdapter;
-  private Spinner              countrySpinner;
-  private TextView             countryCode;
-  private TextView             number;
-  private Button               createButton;
-  private Button               skipButton;
-
   private MasterSecret masterSecret;
 
   @Override
   public void onCreate(Bundle icicle) {
     super.onCreate(icicle);
-    setContentView(R.layout.registration_activity);
-
-    getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME);
-
-    initializeResources();
-    initializeSpinner();
-    initializeNumber();
-  }
-
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == PICK_COUNTRY && resultCode == RESULT_OK && data != null) {
-      this.countryCode.setText(data.getIntExtra("country_code", 1)+"");
-      setCountryDisplay(data.getStringExtra("country_name"));
-      setCountryFormatter(data.getIntExtra("country_code", 1));
-    }
-  }
-
-  private void initializeResources() {
-    this.masterSecret   = getIntent().getParcelableExtra("master_secret");
-    this.countrySpinner = (Spinner)findViewById(R.id.country_spinner);
-    this.countryCode    = (TextView)findViewById(R.id.country_code);
-    this.number         = (TextView)findViewById(R.id.number);
-    this.createButton   = (Button)findViewById(R.id.registerButton);
-    this.skipButton     = (Button)findViewById(R.id.skipButton);
-
-    this.countryCode.addTextChangedListener(new CountryCodeChangedListener());
-    this.number.addTextChangedListener(new NumberChangedListener());
-    this.createButton.setOnClickListener(new CreateButtonListener());
-    this.skipButton.setOnClickListener(new CancelButtonListener());
-
-    if (getIntent().getBooleanExtra("cancel_button", false)) {
-      this.skipButton.setVisibility(View.VISIBLE);
-    } else {
-      this.skipButton.setVisibility(View.INVISIBLE);
-    }
-
-    findViewById(R.id.twilio_shoutout).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.addCategory(Intent.CATEGORY_BROWSABLE);
-        intent.setData(Uri.parse("https://twilio.com"));
-        try {
-          startActivity(intent);
-        } catch (ActivityNotFoundException e) {
-          Log.w(TAG,e);
-        }
-      }
-    });
-  }
-
-  private void initializeSpinner() {
-    this.countrySpinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
-    this.countrySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-    setCountryDisplay(getString(R.string.RegistrationActivity_select_your_country));
-
-    this.countrySpinner.setAdapter(this.countrySpinnerAdapter);
-    this.countrySpinner.setOnTouchListener(new View.OnTouchListener() {
-      @Override
-      public boolean onTouch(View v, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_UP) {
-          Intent intent = new Intent(RegistrationActivity.this, CountrySelectionActivity.class);
-          startActivityForResult(intent, PICK_COUNTRY);
-        }
-        return true;
-      }
-    });
-    this.countrySpinner.setOnKeyListener(new View.OnKeyListener() {
-      @Override
-      public boolean onKey(View v, int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER && event.getAction() == KeyEvent.ACTION_UP) {
-          Intent intent = new Intent(RegistrationActivity.this, CountrySelectionActivity.class);
-          startActivityForResult(intent, PICK_COUNTRY);
-          return true;
-        }
-        return false;
-      }
-    });
-  }
-
-  private void initializeNumber() {
-    PhoneNumberUtil numberUtil  = PhoneNumberUtil.getInstance();
-    String          localNumber = Util.getDeviceE164Number(this);
-
-    try {
-      if (!TextUtils.isEmpty(localNumber)) {
-        Phonenumber.PhoneNumber localNumberObject = numberUtil.parse(localNumber, null);
-
-        if (localNumberObject != null) {
-          this.countryCode.setText(String.valueOf(localNumberObject.getCountryCode()));
-          this.number.setText(String.valueOf(localNumberObject.getNationalNumber()));
-        }
+    this.masterSecret = getIntent().getParcelableExtra("master_secret");
+    int gcmStatus = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+    if (gcmStatus != ConnectionResult.SUCCESS) {
+      if (GooglePlayServicesUtil.isUserRecoverableError(gcmStatus)) {
+        GooglePlayServicesUtil.getErrorDialog(gcmStatus, this, 9000).show();
       } else {
-        String simCountryIso = Util.getSimCountryIso(this);
-
-        if (!TextUtils.isEmpty(simCountryIso)) {
-          this.countryCode.setText(numberUtil.getCountryCodeForRegion(simCountryIso)+"");
-        }
+        Dialogs.showAlertDialog(this, getString(R.string.RegistrationActivity_unsupported),
+                                getString(R.string.RegistrationActivity_sorry_this_device_is_not_supported_for_data_messaging));
       }
-    } catch (NumberParseException npe) {
-      Log.w(TAG, npe);
+      return;
     }
-  }
-
-  private void setCountryDisplay(String value) {
-    this.countrySpinnerAdapter.clear();
-    this.countrySpinnerAdapter.add(value);
-  }
-
-  private void setCountryFormatter(int countryCode) {
-    PhoneNumberUtil util = PhoneNumberUtil.getInstance();
-    String regionCode    = util.getRegionCodeForCountryCode(countryCode);
-
-    if (regionCode == null) this.countryFormatter = null;
-    else                    this.countryFormatter = util.getAsYouTypeFormatter(regionCode);
-  }
-
-  private String getConfiguredE164Number() {
-    return PhoneNumberFormatter.formatE164(countryCode.getText().toString(),
-                                           number.getText().toString());
-  }
-
-  private class CreateButtonListener implements View.OnClickListener {
-    @Override
-    public void onClick(View v) {
-      final RegistrationActivity self = RegistrationActivity.this;
-
-      if (TextUtils.isEmpty(countryCode.getText())) {
-        Toast.makeText(self,
-                       getString(R.string.RegistrationActivity_you_must_specify_your_country_code),
-                       Toast.LENGTH_LONG).show();
-        return;
-      }
-
-      if (TextUtils.isEmpty(number.getText())) {
-        Toast.makeText(self,
-                       getString(R.string.RegistrationActivity_you_must_specify_your_phone_number),
-                       Toast.LENGTH_LONG).show();
-        return;
-      }
-
-      final String e164number = getConfiguredE164Number();
-
-      if (!PhoneNumberFormatter.isValidNumber(e164number)) {
-        Dialogs.showAlertDialog(self,
-                             getString(R.string.RegistrationActivity_invalid_number),
-                             String.format(getString(R.string.RegistrationActivity_the_number_you_specified_s_is_invalid),
-                                           e164number));
-        return;
-      }
-
-      int gcmStatus = GooglePlayServicesUtil.isGooglePlayServicesAvailable(self);
-
-      if (gcmStatus != ConnectionResult.SUCCESS) {
-        if (GooglePlayServicesUtil.isUserRecoverableError(gcmStatus)) {
-          GooglePlayServicesUtil.getErrorDialog(gcmStatus, self, 9000).show();
-        } else {
-          Dialogs.showAlertDialog(self, getString(R.string.RegistrationActivity_unsupported),
-                                  getString(R.string.RegistrationActivity_sorry_this_device_is_not_supported_for_data_messaging));
-        }
-        return;
-      }
-
-      AlertDialog.Builder dialog = new AlertDialog.Builder(self);
-      dialog.setTitle(PhoneNumberFormatter.getInternationalFormatFromE164(e164number));
-      dialog.setMessage(R.string.RegistrationActivity_we_will_now_verify_that_the_following_number_is_associated_with_your_device_s);
-      dialog.setPositiveButton(getString(R.string.RegistrationActivity_continue),
-                               new DialogInterface.OnClickListener() {
-                                 @Override
-                                 public void onClick(DialogInterface dialog, int which) {
-                                   Intent intent = new Intent(self, RegistrationProgressActivity.class);
-                                   intent.putExtra("e164number", e164number);
-                                   intent.putExtra("master_secret", masterSecret);
-                                   startActivity(intent);
-                                   finish();
-                                 }
-                               });
-      dialog.setNegativeButton(getString(R.string.RegistrationActivity_edit), null);
-      dialog.show();
-    }
-  }
-
-  private class CountryCodeChangedListener implements TextWatcher {
-    @Override
-    public void afterTextChanged(Editable s) {
-      if (TextUtils.isEmpty(s)) {
-        setCountryDisplay(getString(R.string.RegistrationActivity_select_your_country));
-        countryFormatter = null;
-        return;
-      }
-
-      int countryCode   = Integer.parseInt(s.toString());
-      String regionCode = PhoneNumberUtil.getInstance().getRegionCodeForCountryCode(countryCode);
-
-      setCountryFormatter(countryCode);
-      setCountryDisplay(PhoneNumberFormatter.getRegionDisplayName(regionCode));
-
-      if (!TextUtils.isEmpty(regionCode) && !regionCode.equals("ZZ")) {
-        number.requestFocus();
-      }
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-    }
-  }
-
-  private class NumberChangedListener implements TextWatcher {
-
-    @Override
-    public void afterTextChanged(Editable s) {
-      if (countryFormatter == null)
-        return;
-
-      if (TextUtils.isEmpty(s))
-        return;
-
-      countryFormatter.clear();
-
-      String number          = s.toString().replaceAll("[^\\d.]", "");
-      String formattedNumber = null;
-
-      for (int i=0;i<number.length();i++) {
-        formattedNumber = countryFormatter.inputDigit(number.charAt(i));
-      }
-
-      if (formattedNumber != null && !s.toString().equals(formattedNumber)) {
-        s.replace(0, s.length(), formattedNumber);
-      }
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-  }
-
-  private class CancelButtonListener implements View.OnClickListener {
-    @Override
-    public void onClick(View v) {
-      TextSecurePreferences.setPromptedPushRegistration(RegistrationActivity.this, true);
-      Intent nextIntent = getIntent().getParcelableExtra("next_intent");
-
-      if (nextIntent == null) {
-        nextIntent = new Intent(RegistrationActivity.this, ConversationListActivity.class);
-      }
-
-      startActivity(nextIntent);
-      finish();
-    }
+    Intent intent = new Intent(this, RegistrationProgressActivity.class);
+    intent.putExtra("master_secret", masterSecret);
+    startActivity(intent);
+    finish();
   }
 }
