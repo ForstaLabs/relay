8fc8f369145d41411c6aec960ff76da79b7bb781
John Lewis <jlewis@forsta.io>
2017-08-10 08:59:52 -0600

    Remove unused import

--- src/io/forsta/securesms/util/Util.java
+++ src/io/forsta/securesms/util/Util.java
@@ -57,7 +57,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
