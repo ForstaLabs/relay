815c4f0bd5b18d45f0838e3b98868375c203d7ff
John Lewis <johndlewis13@gmail.com>
2017-09-30 14:55:24 -0600

    Create basic component for selected recipients

--- src/io/forsta/ccsm/components/SelectedRecipient.java
+++ src/io/forsta/ccsm/components/SelectedRecipient.java
@@ -1,6 +1,8 @@
 package io.forsta.ccsm.components;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,10 +16,15 @@ import io.forsta.securesms.R;
 public class SelectedRecipient extends LinearLayout {
   private ImageButton removeButton;
   private TextView selectedTag;
+
   public SelectedRecipient(Context context) {
     super(context);
+  }
 
-    inflate(context, R.layout.new_conversation_selected_recipient, this);
+  public SelectedRecipient(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    inflater.inflate(R.layout.new_conversation_selected_recipient, this, true);
     removeButton = (ImageButton) findViewById(R.id.selected_recipient_remove);
     selectedTag = (TextView) findViewById(R.id.selected_recipient_tag);
   }
