10b8171301eaefaacd40e17dd362cc6a0b0365fe
Justin Mayfield <tooker@gmail.com>
2017-10-20 15:06:13 -0600

    Handle legacy sessions (migrate them).
    Old sessions with device Id of 1 were saved without their deviceId. We
    now detect this and migrate the file (move it).

--- src/io/forsta/securesms/crypto/storage/TextSecureSessionStore.java
+++ src/io/forsta/securesms/crypto/storage/TextSecureSessionStore.java
@@ -173,8 +173,24 @@ public class TextSecureSessionStore implements SessionStore {
     }
   }
 
-  private File getSessionFile(SignalProtocolAddress address) {
-    return new File(getSessionDirectory(), getSessionName(address));
+  private File getSessionFile(SignalProtocolAddress signalAddr) {
+    if (signalAddr.getDeviceId() == 1) {
+      // Look to see if we need to migrate a legacy session file (e.g. sans-deviceId).
+      File legacyFile = new File(getSessionDirectory(),
+                                 getSessionName(signalAddr, /*legacy*/ true));
+      if (legacyFile.exists()) {
+        File newFile = new File(getSessionDirectory(),
+                                getSessionName(signalAddr, /*legacy*/ false));
+        if (!legacyFile.renameTo(newFile)) {
+          Log.w(TAG, "Failed to migrate/rename legacy session");
+          return legacyFile;
+        } else {
+          Log.w(TAG, "Migrated legacy session to fully qualified name");
+          return newFile;
+        }
+      }
+    }
+    return new File(getSessionDirectory(), getSessionName(signalAddr));
   }
 
   private File getSessionDirectory() {
@@ -190,9 +206,17 @@ public class TextSecureSessionStore implements SessionStore {
   }
 
   private String getSessionName(SignalProtocolAddress signalAddr) {
+    return getSessionName(signalAddr, /*legacy*/ false);
+  }
+
+  private String getSessionName(SignalProtocolAddress signalAddr, boolean legacy) {
     Recipient recipient   = RecipientFactory.getRecipientsFromString(context, signalAddr.getName(), true)
                                             .getPrimaryRecipient();
-    return recipient.getRecipientId() + "." + signalAddr.getDeviceId();
+    if (legacy && signalAddr.getDeviceId() == 1) {
+        return String.valueOf(recipient.getRecipientId());
+    } else {
+        return recipient.getRecipientId() + "." + signalAddr.getDeviceId();
+    }
   }
 
   private @Nullable SignalProtocolAddress getAddressName(File sessionFile) {
