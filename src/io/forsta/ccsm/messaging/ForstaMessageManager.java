01591446d12c9c39d06992c72c834d4dceebbde7
John D Lewis <jlewis@forsta.io>
2018-07-24 09:03:16 -0600

    Update vote and message ref fields in forsta message model

--- src/io/forsta/ccsm/messaging/ForstaMessageManager.java
+++ src/io/forsta/ccsm/messaging/ForstaMessageManager.java
@@ -166,17 +166,17 @@ public class ForstaMessageManager {
           }
         }
 
-        // This is a special case. Message type is CONTENT,
-        // but processing like a control message because
-        // we don't want to save these messages to the message table.
-        if (data.has("vote")) {
-          if (data.has("messageRef")) {
-            String messageId = data.getString("messageRef");
-            // Override message type.
+        if (data.has("messageRef")) {
+          String messageId = data.getString("messageRef");
+          forstaMessage.setMessageRef(messageId);
+          if (data.has("vote")) {
+            // This is a special case. Message type is CONTENT,
+            // but processing like a control message because
+            // we don't want to save these messages to the message table.
             forstaMessage.setMessageType(ForstaMessage.MessageTypes.CONTROL);
             forstaMessage.setControlType(ForstaMessage.ControlTypes.UP_VOTE);
             int vote = data.getInt("vote");
-            forstaMessage.setMessageVote(messageId, vote);
+            forstaMessage.addVote(vote);
             Log.w(TAG, "Message UpVote message: " + messageId + " vote: " + vote);
           }
         }
