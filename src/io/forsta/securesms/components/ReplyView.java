f46874d3980a44a6a0aeccde208a6a39c1938c17
JasonYeh1 <40397851+JasonYeh1@users.noreply.github.com>
2018-07-26 16:29:55 -0600

    Added ReplyView and corresponding xmls

--- src/io/forsta/securesms/components/ReplyView.java
+++ src/io/forsta/securesms/components/ReplyView.java
@@ -1,4 +1,104 @@
 package io.forsta.securesms.components;
 
-public class ReplyView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Attr;
+
+import io.forsta.securesms.R;
+import io.forsta.securesms.recipients.Recipient;
+import io.forsta.securesms.util.Util;
+
+public class ReplyView extends LinearLayout {
+
+    private View            rootView;
+    private TextView        bodyView;
+    private AvatarImageView contactPhoto;
+    private TextView        voteCount;
+
+    private long      id;
+    private Recipient author;
+    private String    body;
+    private int       vote;
+
+
+    public ReplyView(Context context) {
+        super(context);
+        initialize(null);
+    }
+
+    public ReplyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(attrs);
+    }
+
+    public ReplyView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    private void initialize(@Nullable AttributeSet attrs) {
+        inflate(getContext(), R.layout.reply_view, this);
+
+        this.bodyView     = findViewById(R.id.reply_text);
+        this.contactPhoto = findViewById(R.id.reply_contact_photo);
+        this.voteCount  = findViewById(R.id.reply_vote);
+    }
+
+    public void setReply(long id, Recipient author, String body, int vote) {
+        this.id     = id;
+        this.author = author;
+        this.body   = body;
+        this.vote   = vote;
+
+        setReplyAvatar(author);
+        setReplyText(body);
+        setReplyVote(vote);
+    }
+
+    public void dismiss() {
+
+        this.id = 0;
+        this.author = null;
+        this.body = null;
+        this.vote = 0;
+
+        setVisibility(View.GONE);
+    }
+
+    private void setReplyAvatar(Recipient author) {
+        contactPhoto.setVisibility(View.VISIBLE);
+        contactPhoto.setAvatar(author,true);
+    }
+
+    private void setReplyText(String body) {
+        bodyView.setVisibility(View.VISIBLE);
+        bodyView.setText(body == null ? "" : body);
+    }
+
+    private void setReplyVote(int vote) {
+        voteCount.setVisibility(View.VISIBLE);
+        voteCount.setText("(" + vote + ")" );
+    }
+
+    public Recipient getReplyAuthor() {
+        return author;
+    }
+
+    public int getVote() {
+        return vote;
+    }
+
+    public String getReplyBody() {
+        return body;
+    }
+
+    public long getReplyId() {
+        return id;
+    }
 }
