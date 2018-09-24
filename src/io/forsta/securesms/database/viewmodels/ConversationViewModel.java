32acc399eadd01c19a9d41c4208e50a9398c291e
John D Lewis <jlewis@forsta.io>
2018-09-24 10:46:22 -0600

    Create viewmodel for converstations

---
+++ src/io/forsta/securesms/database/viewmodels/ConversationViewModel.java
@@ -0,0 +1,11 @@
+package io.forsta.securesms.database.viewmodels;
+
+import android.arch.lifecycle.ViewModel;
+
+public class ConversationViewModel extends ViewModel {
+
+  @Override
+  protected void onCleared() {
+
+  }
+}
