473aa5021e770fa55d6ab7d3c5a076e196e61672
Justin Mayfield <tooker@gmail.com>
2019-01-04 14:55:34 -0700

    Migrate herokuapp urls to forsta.io ones.
    We never should have used urls we don't control the DNS for.  This is
    required for us to move our infra off heroku seamlessly.

--- src/io/forsta/securesms/push/TextSecureCommunicationFactory.java
+++ src/io/forsta/securesms/push/TextSecureCommunicationFactory.java
@@ -7,8 +7,27 @@ import io.forsta.securesms.util.TextSecurePreferences;
 
 public class TextSecureCommunicationFactory {
 
+  /**
+   * Well, we messed up and used a URL that we don't control the DNS for.  This makes
+   * transitioning infra components next to impossible, so have this code to patch up
+   * our mistake.
+   */
+  private static String getAndUpdateServerUrl(Context context) {
+    String url = TextSecurePreferences.getServer(context);
+    String updatedUrl = null;
+    if (url.matches("https://forsta-signalserver-dev.herokuapp.com")) {
+      updatedUrl = "https://signal-dev.forsta.io";
+    } else if (url.matches("https://forsta-signalserver-prod.herokuapp.com")) {
+      updatedUrl = "https://signal.forsta.io";
+    }
+    if (updatedUrl != null) {
+      TextSecurePreferences.setServer(context, url);
+    }
+    return url;
+  }
+
   public static ForstaServiceAccountManager createManager(Context context) {
-    return new ForstaServiceAccountManager(TextSecurePreferences.getServer(context),
+    return new ForstaServiceAccountManager(getAndUpdateServerUrl(context),
                                            new TextSecurePushTrustStore(context),
                                            TextSecurePreferences.getLocalNumber(context),
                                            TextSecurePreferences.getLocalDeviceId(context),
@@ -18,7 +37,7 @@ public class TextSecureCommunicationFactory {
 
   public static ForstaServiceAccountManager createManager(Context context, String addr,
                                                           String password) {
-    return new ForstaServiceAccountManager(TextSecurePreferences.getServer(context),
+    return new ForstaServiceAccountManager(getAndUpdateServerUrl(context),
                                            new TextSecurePushTrustStore(context),
                                            addr,
                                            new Integer(-1),
